4fc997a8978ca72c1882d2d6ccd42c292f389567
Gabriel-DAloia <g-f-d-m@hotmail.com>
2025-06-09 07:45:32 +0000

    procfile

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,21 @@
+// webpack.prod.js
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: '/'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+  mode: 'production',
+  output: {
+    // En producción, generará /workspaces/FullStack/dist
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new Dotenv({
+      safe: true,
+      systemvars: true
+    })
+  ]
 });

4f1781475ce6b574e44ec233f08caa61934ed0b7
Gabriel-DAloia <g-f-d-m@hotmail.com>
2025-06-09 14:10:00 +0000

    cambios para render

--- src/api/app.py
+++ src/api/app.py
@@ -7,7 +7,6 @@ from flask import Flask, Blueprint, request, jsonify, url_for, send_from_directo
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db 
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
@@ -15,7 +14,6 @@ from api.models import User
 from api.models import Viajes, Top, Belleza, Gastronomia, Category, Reservation, Cart, CartService, Newsletter, Ofertas, Payment, PaymentItem, NewsletterSubscriptions, NewsletterServices
 from api.services import inicializar_servicios
 from dotenv import load_dotenv
-from api.models import db
 from datetime import datetime, timedelta
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager, verify_jwt_in_request
 from flask_bcrypt import Bcrypt
@@ -32,7 +30,6 @@ from itsdangerous import URLSafeTimedSerializer
 from api.models import db, User, PasswordResetToken
 from api.mail_service import MailService
 from sqlalchemy import extract
-from flask_migrate import Migrate
 import json
 
 
@@ -66,6 +63,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 migrate = Migrate(app, db)
+
+with app.app_context():
+    db.create_all()
 
 datos = {
     "newsletter": []
